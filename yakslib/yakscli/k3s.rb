d9ba524e726cb52aca641fc854131d52ce360dca
Jason Young <jay@outfielding.net>
2020-11-01 15:31:35 -0500

    update for moves

--- yakslib/yakscli/k3s.rb
+++ yakslib/yakscli/k3s.rb
@@ -6,21 +6,21 @@ module YaksCLI
     desc "download", "Download k3s Release"
     method_option :force, :type => :boolean, :default => false, :desc => "Force download even if target file(s) exist"
     def download(release_version)
-      require_relative '../labcommands/k3s/download'
+      require_relative '../labtools/_commands/k3s/download'
       command_options = options.merge(cache_directory: Yaks.settings.k3s_cache_directory)
       LabCommand::K3s::Download.new(version: release_version, options: command_options).🚀
     end
 
     desc "latest", "Latest K3s Release Information"
     def latest
-      require_relative '../labcommands/k3s/latest'
+      require_relative '../labtools/_commands/k3s/latest'
       LabCommand::K3s::Latest.new(options: options).🚀
     end
 
     desc "dump DUMPFILE", "Dump K3s Release Information to a yaml file"
     method_option :quiet, :type => :boolean, :default => false, :desc => "Don't show progress spinner"
     def dump(dump_file)
-      require_relative '../labcommands/k3s/dump'
+      require_relative '../labtools/_commands/k3s/dump'
       LabCommand::K3s::Dump.new(dump_file: dump_file, options: options).🚀
     end
 
